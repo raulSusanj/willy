5ed46b697c39917df6dee34807626f8edeae5ac2
raulsusanj <rsusanj@dickeys.com>
2025-03-26 15:11:10 +0100

    change: Adds some layouts for fun

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Employees() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>This is the Willy vaccation app</h1>
+      <h1>This is the employees page.</h1>
     </div>
   );
 }
